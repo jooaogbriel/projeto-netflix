c4e6edc4f8f6d2d004bce24ea70c336dd5df1452
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-09-21 23:01:06 -0300

    featured-gerada

--- src/App.js
+++ src/App.js
@@ -18,8 +18,9 @@ function App()  {
       let originals = list.filter(i=>i.slug === 'originals');
       let randomChosen = Math.floor(Math.random()*(originals[0].itens.results.length -1));
       let chosen = originals[0].itens.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv')
+      setFeaturedData(chosenInfo)
 
-      console.log(chosen)
     }
 
     loadAll();
