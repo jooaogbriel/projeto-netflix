3119f0ad8ae831d6eb2ed1f68ddc75ddc74f5fbb
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-09-22 22:42:19 -0300

    featured-css feitos, componente header criado

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -18,16 +18,16 @@ function FeaturedMovie({item}){
             <div className="featured--vertical">
                 <div className="featured--horizontal" >
                     <div className="featured--name">{item.original_name}</div>
-                <div className="featured--info" >
-                    <div className="featured--points">{item.vote_average}</div>
-                    <div className="featured--year">{firstDate.getFullYear()}</div>
-                    <div className="featured--seasons">{item.number_of_seasons}temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
-                </div>
-                <div className="fatured--description" >{item.overview} </div>
-                <div className="featured--buttons" >
-                        <a href={`/watch/${item.id}`}>▶ Assistir</a>
-                        <a href={`/list/add/${item.id}`}>+ Minha Lista</a>
-                </div>
+                    <div className="featured--info" >
+                        <div className="featured--points">{item.vote_average}</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
+                        <div className="featured--seasons">{item.number_of_seasons}temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                    </div>
+                    <div className="featured--description" >{item.overview} </div>
+                    <div className="featured--buttons" >
+                        <a href={`/watch/${item.id}`} className="featured--watchButton">▶ Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="featured--myListButton">+ Minha Lista</a>
+                    </div>  
                 <div className="featured--genres" ><strong>Gêneros:</strong>{genres.join(', ')}</div>
                 </div>
             </div>
